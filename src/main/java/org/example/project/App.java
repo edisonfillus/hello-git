5654396a90041cad8b4239115570c18c248504f1
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-03-01 22:25:43 -0300

    Some improvements, a file that says hello world!

--- src/main/java/org/example/project/App.java
+++ src/main/java/org/example/project/App.java
@@ -1,14 +1,29 @@
 package org.example.project;
 
+import java.io.IOException;
+
+import org.example.project.util.FileUtil;
+
 /**
- * Hello world!
+ * 
+ * @author Edison Klafke Fillus
+ * Example project to test github
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws  IOException
     {
         System.out.println( "I'm just testing some commits on github!!" );
         System.out.println( "  \\--->> by the way, it's very cool!!" );
+        
+        System.out.println( "Hey, hello.txt, say Hello!" );
+       
+        String filecontent = FileUtil.inputStreamtoString(App.class.getClassLoader().getResourceAsStream("files/hello.txt"));
+                
+        System.out.println(">>"+filecontent);
+       
     }
+    
+    
 }
---
+++ src/main/java/org/example/project/util/FileUtil.java
@@ -0,0 +1,23 @@
+package org.example.project.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FileUtil {
+
+   
+    public static String inputStreamtoString(InputStream inputStream) throws IOException {
+    	ByteArrayOutputStream result = new ByteArrayOutputStream();
+    	byte[] buffer = new byte[1024];
+    	int length;
+    	while ((length = inputStream.read(buffer)) != -1) {
+    	    result.write(buffer, 0, length);
+    	}
+    	// StandardCharsets.UTF_8.name() > JDK 7
+    	return result.toString("UTF-8");
+    }
+    
+    
+	
+}
