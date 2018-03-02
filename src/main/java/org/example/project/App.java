e60ee412d8fdef057d97dedca24a49163e3cc91b
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-03-01 21:02:11 -0300

    Just improving my hello world! Now via eclipse!

--- src/main/java/org/example/project/App.java
+++ src/main/java/org/example/project/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Git World!" );
+        System.out.println( "I'm just testing some commits on github!!" );
+        System.out.println( "  \\--->> by the way, it's very cool!!" );
     }
 }
